456adf6c9e8566bb56f2448a8a23a9119dc1d6f1
Williamparededl1 <wparedesl1995@gmail.com>
2024-09-09 23:42:12 -0500

    Creacion de los siguientes permisos

--- lib/presentation/screens/permissions/permissions_screen.dart
+++ lib/presentation/screens/permissions/permissions_screen.dart
@@ -26,11 +26,39 @@ class _PermissionsView extends ConsumerWidget {
     return ListView(
       children: [
         CheckboxListTile(
+          secondary: const Icon(Icons.camera_alt_outlined),
           value: permissions.cameraGranted,
           title: const Text('Camara'),
           subtitle: Text('Estado ${permissions.camera}'),
           onChanged: (value) {
             ref.read(permissionsProvider.notifier).requestCameraAccess();
+          },
+        ),
+        CheckboxListTile(
+          secondary: const Icon(Icons.photo_album_outlined),
+          value: permissions.photoLibraryGranted,
+          title: const Text('Galeria de Fotos'),
+          subtitle: Text('Estado ${permissions.photoLibrary}'),
+          onChanged: (value) {
+            ref.read(permissionsProvider.notifier).requestPhotoLibraryAccess();
+          },
+        ),
+        CheckboxListTile(
+          secondary: const Icon(Icons.monitor_heart_outlined),
+          value: permissions.sensorsGranted,
+          title: const Text('Sensores'),
+          subtitle: Text('Estado ${permissions.sensors}'),
+          onChanged: (value) {
+            ref.read(permissionsProvider.notifier).requestSensorsAccess();
+          },
+        ),
+        CheckboxListTile(
+          secondary: const Icon(Icons.location_on_outlined),
+          value: permissions.locationGranted,
+          title: const Text('Ubicacion'),
+          subtitle: Text('Estado ${permissions.location}'),
+          onChanged: (value) {
+            ref.read(permissionsProvider.notifier).requestLocationAccess();
           },
         ),
       ],
