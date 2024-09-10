26a1e89749cd6d67c8be8b00131014878f5ae310
Williamparededl1 <wparedesl1995@gmail.com>
2024-09-10 04:46:08 -0500

    change

--- lib/presentation/screens/sensors/compass_screen.dart
+++ lib/presentation/screens/sensors/compass_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:math' show pi;
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:miscelaneos/presentation/provider/providers.dart';
