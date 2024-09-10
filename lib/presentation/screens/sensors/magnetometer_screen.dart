2f58c018c607486f074aced28ce278f800ef2fc0
Williamparededl1 <wparedesl1995@gmail.com>
2024-09-10 02:20:43 -0500

    actualizar el MagnetometerScreen

--- lib/presentation/screens/sensors/magnetometer_screen.dart
+++ lib/presentation/screens/sensors/magnetometer_screen.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:miscelaneos/presentation/provider/providers.dart';
 
-class MagnetometerScreen extends StatelessWidget {
+class MagnetometerScreen extends ConsumerWidget {
   const MagnetometerScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final magnetometro$ = ref.watch(magnetometerProvider);
     return Scaffold(
       appBar: AppBar(
-        title: const Text('MagnetometerScreen'),
+        title: const Text('Magnetometro'),
       ),
-      body: const Center(
-        child: Text('Hola mundo'),
+      body: Center(
+        child: magnetometro$.when(
+            data: (data) => Text(
+                  data.toString(),
+                  style: const TextStyle(fontSize: 30),
+                ),
+            error: (error, stackTrace) => Text('$error'),
+            loading: () => const CircularProgressIndicator()),
       ),
     );
   }
