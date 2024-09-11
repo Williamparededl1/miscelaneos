a524628f6ebde64505964798f933cf1ae0090eb0
Williamparededl1 <wparedesl1995@gmail.com>
2024-09-11 00:11:32 -0500

    terminamos con la vista del pokemon

--- lib/presentation/screens/pokemons/pokemon_screen.dart
+++ lib/presentation/screens/pokemons/pokemon_screen.dart
@@ -1,18 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:miscelaneos/domian/domain.dart';
+import 'package:miscelaneos/presentation/provider/providers.dart';
 
-class PokemonScreen extends StatelessWidget {
+class PokemonScreen extends ConsumerWidget {
   final String pokemonId;
 
   const PokemonScreen({super.key, required this.pokemonId});
 
   @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final pokemonAsync = ref.watch(pokemonProvider(pokemonId));
+
+    return pokemonAsync.when(
+      data: (data) => _PokemonView(pokemon: data),
+      error: (error, stackTrace) => _ErrorWidget(message: error.toString()),
+      loading: () => const _LoadingWidget(),
+    );
+  }
+}
+
+class _PokemonView extends StatelessWidget {
+  final Pokemon pokemon;
+
+  const _PokemonView({required this.pokemon});
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Pokemon <nombre>'),
+        title: Text(pokemon.name),
       ),
       body: Center(
-        child: Text('Pokemon: $pokemonId'),
+        child: Image.network(
+          pokemon.spriteFront,
+          fit: BoxFit.contain,
+          height: 150,
+          width: 150,
+        ),
+      ),
+    );
+  }
+}
+
+class _ErrorWidget extends StatelessWidget {
+  final String message;
+  const _ErrorWidget({required this.message});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(message),
+      ),
+    );
+  }
+}
+
+class _LoadingWidget extends StatelessWidget {
+  const _LoadingWidget();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
       ),
     );
   }
