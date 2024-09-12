14d280d5f115a6146e940bb6645876f0b3915ef7
Williamparededl1 <wparedesl1995@gmail.com>
2024-09-12 15:25:05 -0500

    cargar imagenes de los iconos

--- lib/config/plugins/quick_actions_plugin.dart
+++ lib/config/plugins/quick_actions_plugin.dart
@@ -1,3 +1,4 @@
+import 'package:miscelaneos/config/config.dart';
 import 'package:quick_actions/quick_actions.dart';
 
 class QuickActionsPlugin {
@@ -6,25 +7,29 @@ class QuickActionsPlugin {
     quickActions.initialize((shortcutType) {
       switch (shortcutType) {
         case 'biometric':
+          router.push('/biometric');
           break;
         case 'compass':
+          router.push('/compass');
           break;
         case 'pokemons':
+          router.push('/pokemons');
           break;
         case 'charmander':
+          router.push('/pokemons/4');
           break;
       }
     });
 
     quickActions.setShortcutItems(<ShortcutItem>[
       const ShortcutItem(
-          type: 'biometric', localizedTitle: 'Biometric', icon: 'icon_main'),
+          type: 'biometric', localizedTitle: 'Biometric', icon: 'biometric'),
       const ShortcutItem(
-          type: 'compass', localizedTitle: 'Compass', icon: 'icon_main'),
+          type: 'compass', localizedTitle: 'Compass', icon: 'compass'),
       const ShortcutItem(
-          type: 'pokemons', localizedTitle: 'Pokemons', icon: 'icon_main'),
+          type: 'pokemons', localizedTitle: 'Pokemons', icon: 'pokemons'),
       const ShortcutItem(
-          type: 'charmander', localizedTitle: 'Charmander', icon: 'icon_main'),
+          type: 'charmander', localizedTitle: 'Charmander', icon: 'charmander'),
     ]);
   }
 }
