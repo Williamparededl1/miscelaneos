8c61a7de72d792964cd9864198c710b9f43f1d88
Williamparededl1 <wparedesl1995@gmail.com>
2024-09-11 22:07:29 -0500

    configuracion del FlutterFragmentActivity

--- android/app/src/main/kotlin/com/williamParedes/miselanios/MainActivity.kt
+++ android/app/src/main/kotlin/com/williamParedes/miselanios/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.williamParedes.miselanios
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterFragmentActivity()
